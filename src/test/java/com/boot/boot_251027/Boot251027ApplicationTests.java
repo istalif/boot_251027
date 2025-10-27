3eab05e3f615f07a9b5bbbdfd2156a92bcfe83a2
istalif <bread998@naver.com>
2025-10-27 16:12:29 +0900

    333
    Signed-off-by: istalif <bread998@naver.com>

---
+++ src/test/java/com/boot/boot_251027/Boot251027ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boot.boot_251027;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Boot251027ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
